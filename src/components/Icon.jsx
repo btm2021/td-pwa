77318c9a9a4313749e433061e985907672a45325
unknown <trinhminhbao@gmail.com>
2026-01-21 12:13:33 +0700

    Asd

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -184,11 +184,7 @@ export function Icon({ name, size = 24, className = '' }) {
                 <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" />
             </svg>
         ),
-        check: (
-            <svg viewBox="0 0 24 24" width={size} height={size} fill="currentColor">
-                <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
-            </svg>
-        ),
+
     };
 
     return (
